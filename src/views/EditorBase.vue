56948db4b03c3f260bfc1687f7a3f78b6bcb554c
xem <xemxx@qq.com>
2023-07-21 01:03:55 +0800

    fix: sync note when startup

--- src/views/EditorBase.vue
+++ src/views/EditorBase.vue
@@ -10,14 +10,18 @@ import Sidebar from '@/components/sidebar/index.vue'
 import Editor from '@/components/editor/editor.vue'
 import { usePreferenceStore } from '@/store/preference'
 import { useListenStore } from '@/store/listen'
+import { useSyncStore } from '@/store/sync'
 
 const preference = usePreferenceStore()
 const listen = useListenStore()
+const sync = useSyncStore()
 
 // 监听内容变动
 listen.listenFileSave()
 // 监听偏好设置即时生效
 preference.getLocal()
+
+sync.sync()
 </script>
 <style lang="stylus" scoped>
 .container
