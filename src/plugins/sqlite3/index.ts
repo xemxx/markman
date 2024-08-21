af6623191cdff69530fcbc6f6761442c70e30804
xemxx <1007441364@qq.com>
2024-08-21 11:22:52 +0800

    fix: Add uuid column to user table if not present

--- src/plugins/sqlite3/index.ts
+++ src/plugins/sqlite3/index.ts
@@ -8,23 +8,42 @@ let dbPath = path.join(app.getPath('userData'), 'data.db')
 
 export const db = Sqlite.getInstance(dbPath)
 
+interface tableStruct {
+  cid: number
+  name: string
+  type: string
+  notnull: string
+  dflt_value: any
+  pk: number
+}
+
 export const initDB = async function () {
   for (const row of sql) {
     await db.exec(row)
   }
 
   // 0.3.0 之前版本升级
-  const user =
-    (await db.get<userItem>('select * from user limit 1', {})) || undefined
-  if (user && user.uuid === '' && user.username !== '') {
+  const fileds =
+    (await db.all<tableStruct>('PRAGMA table_info(user)', {})) || undefined
+  let have = false
+  if (!fileds || fileds.length === 0) {
+    return
+  }
+  for (const filed of fileds) {
+    if (filed.name === 'uuid') {
+      have = true
+      break
+    }
+  }
+  if (!have) {
     // 新增uuid
     await db.exec(`ALTER TABLE user ADD COLUMN uuid TEXT;`)
     // 删除无用账户
     await db.exec(`DELETE FROM user
 WHERE id NOT IN (
-    select id from (SELECT max(lastSC),id,username
-    FROM user
-    GROUP BY username)
+select id from (SELECT max(lastSC),id,username
+FROM user
+GROUP BY username)
 )`)
   }
 }
