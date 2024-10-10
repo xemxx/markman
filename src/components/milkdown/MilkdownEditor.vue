2877b1d8a2f871aa8b6584d78ff515dada7da762
xemxx <xemxx@qq.com>
2024-10-11 00:13:48 +0800

    tmp: init

---
+++ src/components/milkdown/MilkdownEditor.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Milkdown, useEditor } from '@milkdown/vue'
+import { defaultValueCtx, Editor, rootCtx } from '@milkdown/kit/core'
+import { nord } from '@milkdown/theme-nord'
+import { commonmark } from '@milkdown/kit/preset/commonmark'
+
+const markdown = `# Milkdown Vue Commonmark
+
+> You're scared of a world where you're needed.
+
+This is a demo for using Milkdown with **Vue**.`
+
+useEditor(root => {
+  return Editor.make()
+    .config(nord)
+    .config(ctx => {
+      ctx.set(rootCtx, root)
+      ctx.set(defaultValueCtx, markdown)
+    })
+    .use(commonmark)
+})
+</script>
+
+<template>
+  <Milkdown />
+</template>
